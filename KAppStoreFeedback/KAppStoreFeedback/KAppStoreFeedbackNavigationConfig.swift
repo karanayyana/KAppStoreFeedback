25ab7bdf2e74ff3c9763f5e0d91f3d80fbd0a0de
Prajwal Karanayyana Vasantha <prajwal.vasantha@sureprep.com>
2018-07-24 12:40:50 -0700

    modifing file structure

---
+++ KAppStoreFeedback/KAppStoreFeedback/KAppStoreFeedbackNavigationConfig.swift
@@ -0,0 +1,13 @@
+//
+//  KAppStoreFeedbackNavigationConfig.swift
+//  KAppStoreFeedback
+//
+//  Created by Prajwal Karanayyana Vasantha on 7/24/18.
+//  Copyright © 2018 karanayyana. All rights reserved.
+//
+
+import UIKit
+
+class KAppStoreFeedbackNavigationConfig: NSObject {
+
+}
---
+++ KAppStoreFeedback/KAppStoreFeedback/KAppStoreFeedbackUIElementsConfig.swift
@@ -0,0 +1,13 @@
+//
+//  KAppStoreFeedbackUIElementsConfig.swift
+//  KAppStoreFeedback
+//
+//  Created by Prajwal Karanayyana Vasantha on 7/24/18.
+//  Copyright © 2018 karanayyana. All rights reserved.
+//
+
+import UIKit
+
+class KAppStoreFeedbackUIElementsConfig: NSObject {
+
+}
